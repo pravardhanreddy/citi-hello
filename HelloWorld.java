4a3b3af810619ad37033070dd7130c8e8c5baa06
pravardhanreddy <pravardhan.green1998@gmail.com>
2020-09-16 11:39:23 +0000

    Checks

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+// Simple Java Program to Print Hello World
 public class HelloWorld {
-	public static void main (String arg[]){
-		System.out.println("Hello to Citi");
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
 	}
 }
+
